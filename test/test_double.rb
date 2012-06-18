14956b56bcefa67db4c583e3a8e97b99d04837c9
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-06-18 19:03:35 +0100

    I don't want to see Kernel::Bar when constant is really missing

--- test/test_double.rb
+++ test/test_double.rb
@@ -18,7 +18,7 @@ class TestDouble < Test::Unit::TestCase
     e = assert_raise NameError do
       Foo.double.baz
     end
-    assert_equal 'uninitialized constant Bar', e.message
+    refute e.message.match /Kernel::Bar/
 
     Kernel.const_set :Bar, Module.new
     assert_raise NoMethodError do
